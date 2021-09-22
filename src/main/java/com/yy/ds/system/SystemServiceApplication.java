d6bd96954291a21a951f67ed266b47bfc2dbba9a
滕勇 <tengyong@qianxin.com>
2021-09-22 18:29:42 +0800

    swagger2升级为swagger3

--- src/main/java/com/yy/ds/system/SystemServiceApplication.java
+++ src/main/java/com/yy/ds/system/SystemServiceApplication.java
@@ -2,12 +2,16 @@ package com.yy.ds.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class SystemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SystemServiceApplication.class, args);
+    }
 
 }
